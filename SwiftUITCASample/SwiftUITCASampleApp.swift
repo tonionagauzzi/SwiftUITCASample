93ad34e9efb275cbf37879c1ed1412cb0a7eda3d
nagauzzi <kensaku.nagauchi@access-company.com>
2021-06-11 22:16:27 +0900

    Initial Commit

---
+++ SwiftUITCASample/SwiftUITCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITCASampleApp.swift
+//  SwiftUITCASample
+//
+//  Created by K Nagauchi on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
